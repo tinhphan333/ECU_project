e15068a80700d0828ca383bd8c9a5f69a2072803
phantinhphan <phantinhphan333.work@gmail.com>
2025-08-19 00:51:04 +0700

    Part 2

--- IOHwAb/IOHwAb_Adc.c
+++ IOHwAb/IOHwAb_Adc.c
@@ -1,1 +1,79 @@
+/**
+ * @file    IOHwAb_Adc.c
+ * @brief   Định nghĩa các hàm để đọc và chuyển đổi giá trị các cảm biến từ kênh ADC.
+ * @details File chứa các hàm để đọc và chuyển đổi giá trị các cảm biến từ kênh ADC bao gồm
+ *          đọc nhiệt độ, điện áp, dòng điện, moment xoắn và vòng tua máy.
+ * @author  phantinhphan333
+ * @date    2025-08-10
+ * @version 1.0
+ */
+
 #include "IOHwAb_Adc.h"
+
+Std_ReturnType IoHwAb_ReadTemp(uint16_t *temp_adc, float *temp_C)
+{
+    // Kiểm tra con trỏ ADC truyền vào
+    if (temp_adc == NULL)
+    {
+        printf("Lỗi đọc cảm biến nhiệt độ\n");
+        return E_NOT_OK;
+    }
+
+    *(temp_adc) = Adc_ReadChannel(TEMP);                     // Đọc giá trị từ kênh ADC và gán giá trị cho biến
+    *(temp_C) = (float)*(temp_adc)*TEMP_MAX / ADC_12BIT_MAX; // Chuyển đổi giá trị sang độ C
+    return E_OK;
+}
+
+Std_ReturnType IoHwAb_ReadVoltage(uint16_t *voltage_adc, float *voltage_V)
+{
+    // Kiểm tra con trỏ ADC truyền vào
+    if (voltage_adc == NULL)
+    {
+        printf("Lỗi đọc cảm biến điện áp\n");
+        return E_NOT_OK;
+    }
+
+    *(voltage_adc) = Adc_ReadChannel(VOLTAGE);                        // Đọc giá trị từ kênh ADC và gán giá trị cho biến
+    *(voltage_V) = (float)*(voltage_adc)*VOLTAGE_MAX / ADC_12BIT_MAX; // Chuyển đổi giá trị sang Volt
+    return E_OK;
+}
+
+Std_ReturnType IoHwAb_ReadCurrent(uint16_t *current_adc, float *current_A)
+{
+    // Kiểm tra con trỏ ADC truyền vào
+    if (current_adc == NULL)
+    {
+        printf("Lỗi đọc cảm biến dòng điện\n");
+        return E_NOT_OK;
+    }
+
+    *(current_adc) = Adc_ReadChannel(CURRENT);                        // Đọc giá trị từ kênh ADC và gán giá trị cho biến
+    *(current_A) = (float)*(current_adc)*CURRENT_MAX / ADC_12BIT_MAX; // Chuyển đổi giá trị sang Ampe
+    return E_OK;
+}
+
+Std_ReturnType IoHwAb_ReadTorque(uint16_t *torque_adc, float *torque_Nm)
+{
+    // Kiểm tra con trỏ ADC truyền vào
+    if (torque_adc == NULL)
+    {
+        printf("Lỗi đọc cảm biến moment xoắn\n");
+        return E_NOT_OK;
+    }
+
+    *(torque_adc) = Adc_ReadChannel(TORQUE);                        // Đọc giá trị từ kênh ADC và gán giá trị cho biến
+    *(torque_Nm) = (float)*(torque_adc)*TORQUE_MAX / ADC_12BIT_MAX; // Chuyển đổi giá trị sang Nm
+    return E_OK;
+}
+
+Std_ReturnType IoHwAb_ReadRpm(uint16_t *rpm)
+{
+    // Kiểm tra con trỏ ADC truyền vào
+    if (rpm == NULL)
+    {
+        printf("Lỗi đọc cảm biến vòng tua máy\n");
+        return E_NOT_OK;
+    }
+    *(rpm) = Adc_ReadChannel(RPM); // Đọc giá trị từ kênh ADC và gán giá trị cho biến
+    return E_OK;
+}
--- IOHwAb/IOHwAb_Adc.h
+++ IOHwAb/IOHwAb_Adc.h
@@ -1,51 +1,66 @@
+/**
+ * @file    IOHwAb_Adc.h
+ * @brief   Khai báo các hàm để đọc và chuyển đổi giá trị các cảm biến từ kênh ADC.
+ * @details File cung cấp giao diện để đọc và chuyển đổi giá trị các cảm biến từ kênh ADC bao gồm
+ *          đọc nhiệt độ, điện áp, dòng điện, moment xoắn và vòng quay.
+ * @author  phantinhphan333
+ * @date    2025-08-10
+ * @version 1.0
+ */
+
 #ifndef IOHWAB_ADC_H
 #define IOHWAB_ADC_H
 
-#include <stdint.h>
+#include "MCAL/Adc/Adc.h"
 
-// Khai báo kiểu trả về cho các hàm ADC
+#define ADC_12BIT_MAX 4095 // Định nghĩa giá trị lớn nhất của ADC
+#define TEMP_MAX 200       // Giới hạn nhiệt độ là 200 độ C
+#define VOLTAGE_MAX 60     // Giới hạn điện áp là 60 V
+#define CURRENT_MAX 50     // Giới hạn cường độ dòng điện là 50 A
+#define TORQUE_MAX 120     // Giới hạn moment xoắn là 120 Nm
+#define RPM_MAX
 typedef enum
 {
-    ADC_OK = 0,     // Trả về khi thực hiện thành công
-    ADC_ERROR = -1, // Trả về khi có lỗi xảy ra
+    E_OK,    // Trả về khi hàm thực hiện thành công
+    E_NOT_OK // Trả về khi có lỗi xảy ra
 } Std_ReturnType;
 
 /**************************************************************************
- * @brief   Đọc giá trị nhiệt độ từ ADC.
+ * @brief   Đọc và chuyển đổi giá trị nhiệt độ từ ADC.
  * @param   temp_adc  Con trỏ đến biến lưu giá trị ADC.
  * @param   temp_C    Con trỏ đến biến lưu giá trị nhiệt độ tính bằng độ C.
- * @return  Trả về ADC_OK nếu thành công, ngược lại trả về ADC_ERROR.
+ * @return  Trả về E_OK nếu thành công, ngược lại trả về E_NOT_OK.
  **************************************************************************/
 Std_ReturnType IoHwAb_ReadTemp(uint16_t *temp_adc, float *temp_C);
 
 /**************************************************************************
- * @brief   Đọc giá trị điện áp từ ADC.
+ * @brief   Đọc và chuyển đổi giá trị điện áp từ ADC.
  * @param   voltage_adc  Con trỏ đến biến lưu giá trị ADC.
  * @param   voltage_V    Con trỏ đến biến lưu giá trị điện áp tính bằng Volt.
- * @return  Trả về ADC_OK nếu thành công, ngược lại trả về ADC_ERROR.
+ * @return  Trả về E_OK nếu thành công, ngược lại trả về E_NOT_OK.
  **************************************************************************/
 Std_ReturnType IoHwAb_ReadVoltage(uint16_t *voltage_adc, float *voltage_V);
 
 /**************************************************************************
- * @brief   Đọc giá trị dòng điện từ ADC.
+ * @brief   Đọc và chuyển đổi giá trị dòng điện từ ADC.
  * @param   current_adc  Con trỏ đến biến lưu giá trị ADC.
  * @param   current_A    Con trỏ đến biến lưu giá trị dòng điện tính bằng Ampe.
- * @return  Trả về ADC_OK nếu thành công, ngược lại trả về ADC_ERROR.
+ * @return  Trả về E_OK nếu thành công, ngược lại trả về E_NOT_OK.
  **************************************************************************/
 Std_ReturnType IoHwAb_ReadCurrent(uint16_t *current_adc, float *current_A);
 
 /**************************************************************************
- * @brief   Đọc giá trị moment xoắn từ ADC.
+ * @brief   Đọc và chuyển đổi giá trị moment xoắn từ ADC.
  * @param   torque_adc  Con trỏ đến biến lưu giá trị ADC.
  * @param   torque_Nm    Con trỏ đến biến lưu giá trị dòng điện tính bằng Nm.
- * @return  Trả về ADC_OK nếu thành công, ngược lại trả về ADC_ERROR.
+ * @return  Trả về E_OK nếu thành công, ngược lại trả về E_NOT_OK.
  **************************************************************************/
 Std_ReturnType IoHwAb_ReadTorque(uint16_t *torque_adc, float *torque_Nm);
 
 /**************************************************************************
- * @brief   Đọc giá trị vòng quay từ ADC.
+ * @brief   Đọc và chuyển đổi giá trị vòng quay từ ADC.
  * @param   rpm      Con trỏ đến biến lưu giá trị vòng quay tính bằng RPM.
- * @return  Trả về ADC_OK nếu thành công, ngược lại trả về ADC_ERROR.
+ * @return  Trả về E_OK nếu thành công, ngược lại trả về E_NOT_OK.
  **************************************************************************/
 Std_ReturnType IoHwAb_ReadRpm(uint16_t *rpm);
 
